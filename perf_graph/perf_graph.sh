030d3725eb2c66a08fec54233368ea06e1d27013
skippor <1935033572@qq.com>
2023-03-18 23:07:17 +0800

    update

--- perf_graph/perf_graph.sh
+++ perf_graph/perf_graph.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+FLAME_GRAPH_DIR=../FlameGraph
+
+cur_dir=`date "+%F_%T"`
+mkdir -p ${cur_dir}
+
+echo "Stack record ..."
+perf record -C 0 -p `pidof nginx` -o "./${cur_dir}/perf.dat" -- sleep 30
+perf script -i "./${cur_dir}/perf.dat" > "./${cur_dir}/perf.unflold"
+
 echo "Stack collapse ..."
 ../stackcollapse-perf.pl "./${cur_dir}/perf.unflold" > "./${cur_dir}/perf.folded"
+
 echo "Flame graph ..."
 ../flamegraph.pl "./${cur_dir}/perf.folded" > "./${cur_dir}/perf.svg"
 
