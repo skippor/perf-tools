7dc1f632426ccf127246c1e144d2616cb83aa019
skippor <1935033572@qq.com>
2023-03-19 14:52:48 +0800

    update

--- perf_graph/perf_graph.sh
+++ perf_graph/perf_graph.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-FLAME_GRAPH_DIR=../FlameGraph
+FLAME_GRAPH_DIR=./FlameGraph
+WEB_PERF_DIR=./
 
 cur_dir=`date "+%F_%T"`
 mkdir -p ${cur_dir}
 
-echo "Stack record ..."
-perf record -C 0 -p `pidof nginx` -o "./${cur_dir}/perf.dat" -- sleep 30
+echo "Perf record ..."
+#perf record -C 0 -p `pidof -s nginx` -o "./${cur_dir}/perf.dat" -- sleep 30
+perf record -g -F 99 -o "./${cur_dir}/perf.dat" -- sleep 30
 perf script -i "./${cur_dir}/perf.dat" > "./${cur_dir}/perf.unflold"
 
 echo "Stack collapse ..."
-../stackcollapse-perf.pl "./${cur_dir}/perf.unflold" > "./${cur_dir}/perf.folded"
+${FLAME_GRAPH_DIR}/stackcollapse-perf.pl "./${cur_dir}/perf.unflold" > "./${cur_dir}/perf.folded"
 
 echo "Flame graph ..."
-../flamegraph.pl "./${cur_dir}/perf.folded" > "./${cur_dir}/perf.svg"
+${FLAME_GRAPH_DIR}/flamegraph.pl "./${cur_dir}/perf.folded" > "./${cur_dir}/perf.svg"
 
+cp "./${cur_dir}/perf.svg" "${WEB_PERF_DIR}/perf-${cur_dir}.svg"
